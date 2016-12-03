c2a87cbb890906837dbb6ef0c36ac63c75843e95
davidtnfsh <davidtnfsh@gmail.com>
2016-12-03 17:38:10 +0800

    api可以依照語言參數去提供多語系的kcm

--- KCM_web_api/views.py
+++ KCM_web_api/views.py
@@ -4,17 +4,19 @@ from djangoApiDec.djangoApiDec import queryString_required
 from KCM.KCM import KCM
 import os
 
-@queryString_required(['keyword'])
+@queryString_required(['lang', 'keyword', 'num'])
 def kcmApi(request):
 	"""Generate list of term data source files
 	Returns:
 		if contains invalid queryString key, it will raise exception.
 	"""
-	kcmObject = KCM('model', 'KCM/')
 	keyword = request.GET['keyword']
-	filePath = kcmObject.getFilePath('cht')
+	lang = request.GET['lang']
+	num = request.GET['num']
+	kcmObject = KCM('model', 'KCM/')
+	filePath = kcmObject.getFilePath(lang)
 	kcmObject.setMissionType('json')
 	pq = kcmObject.get_cor_term_freq_pq(filePath, keyword, 1)
-	result = kcmObject.getOrCreate(keyword, kcmObject.return_top_n_cor_terms, pq, 10)
+	result = kcmObject.getOrCreate(keyword, kcmObject.return_top_n_cor_terms, pq, num)
 
 	return JsonResponse(result, safe=False)
