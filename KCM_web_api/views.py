cc16344cf4939188f59c61d1f81f7c186e80b0c8
davidtnfsh <davidtnfsh@gmail.com>
2016-12-07 15:01:39 +0800

    新增num參數，再Query api的時候可以指定要回傳幾個相關的詞

--- KCM_web_api/views.py
+++ KCM_web_api/views.py
@@ -12,11 +12,11 @@ def kcmApi(request):
 	"""
 	keyword = request.GET['keyword']
 	lang = request.GET['lang']
-	num = request.GET['num']
-	kcmObject = KCM('model', 'KCM/')
-	filePath = kcmObject.getFilePath(lang)
+	num = int(request.GET['num'])
+	kcmObject = KCM( num, 'model', 'KCM/')
+	model = kcmObject.getFilePath(lang)
 	kcmObject.setMissionType('json')
-	pq = kcmObject.get_cor_term_freq_pq(filePath, keyword, 1)
-	result = kcmObject.getOrCreate(keyword, kcmObject.return_top_n_cor_terms, pq, num)
+	pq = kcmObject.get_cor_term_freq_pq(model, keyword, 1)
+	result = kcmObject.getOrCreate(keyword, kcmObject.return_top_n_cor_terms, pq)
 
 	return JsonResponse(result, safe=False)
