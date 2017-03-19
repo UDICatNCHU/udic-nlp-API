56127ed256890e954298a03b830a620d6897458c
davidtnfsh <davidtnfsh@gmail.com>
2017-03-19 19:51:22 +0800

    remove KCEM and update setting.py etc.

--- KCM_web_api/urls.py
+++ KCM_web_api/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from KCM_web_api import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/kcm/$', views.kcm, name='kcm'), 
-    url(r'^api/kem/$', views.kem, name='kem'),
-    url(r'^api/kcem/$', views.kcem, name='kcem'), 
+    # url(r'^api/kcm/$', views.kcm, name='kcm'), 
+    # url(r'^api/kem/$', views.kem, name='kem'),
+]
+
+# KCEM
+
+import kcem.urls
+urlpatterns += [
+    url(r'^kcem/', include(kcem.urls))
 ]
--- KCM_web_api/views.py
+++ KCM_web_api/views.py
@@ -1,46 +1,32 @@
-# -*- coding: utf-8 -*-
-from django.http import JsonResponse
-from djangoApiDec.djangoApiDec import queryString_required
-from KCM.build.import2DB import import2Mongo
-from KEM.KEM import KEM
-from KCEM.KCEM import KCEM
-from .settings_database import uri
-import os
+# # -*- coding: utf-8 -*-
+# from django.http import JsonResponse
+# from djangoApiDec.djangoApiDec import queryString_required
+# from KCM.__main__ import KCM
+# from KEM.KEM import KEM
+# from KCEM.KCEM import KCEM
+# from .settings_database import uri
+# import os
 
-@queryString_required(['lang', 'keyword'])
-def kcm(request):
-    """Generate list of term data source files
-    Returns:
-        if contains invalid queryString key, it will raise exception.
-    """
-    keyword = request.GET['keyword']
-    lang = request.GET['lang']
+# @queryString_required(['lang', 'keyword'])
+# def kcm(request):
+#     """Generate list of term data source files
+#     Returns:
+#         if contains invalid queryString key, it will raise exception.
+#     """
+#     keyword = request.GET['keyword']
+#     lang = request.GET['lang']
 
-    i = import2Mongo(lang, uri)
-    result = i.get(keyword, int(request.GET['num']) if 'num' in request.GET else 10)
-    return JsonResponse(result, safe=False)
+#     i = KCM(lang, uri)
+#     result = i.get(keyword, int(request.GET['num']) if 'num' in request.GET else 10)
+#     return JsonResponse(result, safe=False)
 
-@queryString_required(['lang', 'keyword'])
-def kem(request):
-    """Generate list of term data source files
-    Returns:
-        if contains invalid queryString key, it will raise exception.
-    """
-    keyword = request.GET['keyword']
-    lang = request.GET['lang']
-    kemObject = KEM( int(request.GET['num']) if 'num' in request.GET else 10, lang, 'KEM/', uri)
-    return JsonResponse(kemObject.getTerms(keyword, int(request.GET['num']) if 'num' in request.GET else 10), safe=False)
-
-@queryString_required(['lang', 'keyword', 'kcm', 'kem'])
-def kcem(request):
-    import json, requests
-    """Generate list of term data source files
-    Returns:
-        if contains invalid queryString key, it will raise exception.
-    """
-    keyword = request.GET['keyword']
-    lang = request.GET['lang']
-    kcm = request.GET['kcm']
-    kem = request.GET['kem']
-    k = KCEM(uri)
-    return JsonResponse(k.get(keyword, lang, num = int(request.GET['num']) if 'num' in request.GET else 10, kem_topn_num=kem, kcm_topn_num=kcm), safe=False)
+# @queryString_required(['lang', 'keyword'])
+# def kem(request):
+#     """Generate list of term data source files
+#     Returns:
+#         if contains invalid queryString key, it will raise exception.
+#     """
+#     keyword = request.GET['keyword']
+#     lang = request.GET['lang']
+#     kemObject = KEM( int(request.GET['num']) if 'num' in request.GET else 10, lang, 'KEM/', uri)
+#     return JsonResponse(kemObject.getTerms(keyword, int(request.GET['num']) if 'num' in request.GET else 10), safe=False)
