310361e21ee7a67b34a1c4db3a05f28bfc1e9ce0
david <davidtnfsh@gmail.com>
2017-08-10 15:51:13 +0800

    幾乎完成api頁面了

---
+++ assets/js/ApiDemoModal.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {Table, Menu, Label, Input, Button, Header, Image, Modal, Icon } from 'semantic-ui-react'
+
+const ResultTable = (props) => {
+  return (
+    <Table celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>單字</Table.HeaderCell>
+          <Table.HeaderCell>分數</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+      	{
+      		props.result.map((item, idx) => (
+		        <Table.Row key={idx}>
+		          <Table.Cell>{item[0]}</Table.Cell>
+		          <Table.Cell>{item[1]}</Table.Cell>
+		        </Table.Row>
+    			))
+      	}
+      </Table.Body>
+    </Table>
+  )
+}
+
+class InputExampleInput extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	  	result:[]
+	  }
+  	this.handleKeypress = this.handleKeypress.bind(this)
+	}
+
+	handleKeypress(event){
+	  if(event.key == 'Enter'){
+			$.getJSON( "http://140.120.13.244:10000/kem/?keyword=" + event.target.value + "&lang=cht", (result) => {
+				this.setState({result})
+			})
+	  }
+	}
+
+	render() {
+		return(
+			<div>
+			  <Input placeholder='Search...' onKeyUp={this.handleKeypress}/>
+			  <ResultTable result={this.state.result}/>
+			</div>	
+		)
+	}
+}
+
+const ApiDemoModal = (props) => (
+	<div>
+	  <Header as='h3' style={{ fontSize: '2em' }}>{props.name}</Header>
+	  <p style={{ fontSize: '1.33em' }}>
+	    {props.intro}
+	  </p>
+	  <Modal trigger={<Button>試用API</Button>}>
+	    <Modal.Header>{props.name}</Modal.Header>
+	    <Modal.Content image>
+	      <Image wrapped size='medium' src={props.picture} />
+	      <Modal.Description>
+	        <Header>使用說明</Header>
+	        <p>{props.descript}</p>
+	        <InputExampleInput/>
+	      </Modal.Description>
+	    </Modal.Content>
+	  </Modal>
+	</div>
+)
+
+export default ApiDemoModal
--- assets/js/api.jsx
+++ assets/js/api.jsx
@@ -16,6 +16,7 @@ import {
 } from 'semantic-ui-react'
 import Footer from './footer.jsx'
 import FixedMenu from './FixedMenu.jsx'
+import ApiDemoModal from './ApiDemoModal.jsx'
 
 export default class HomepageLayout extends Component {
   constructor(props) {
@@ -67,17 +68,14 @@ export default class HomepageLayout extends Component {
 
         <Segment style={{ padding: '8em 0em' }} vertical>
           <Container text>
-            <Header as='h3' style={{ fontSize: '2em' }}>研究動機</Header>
-            <p style={{ fontSize: '1.33em' }}>
-              我們實驗室主要於資料庫查詢處理與資料探勘技術領域進行研究；各種不同資訊平台及其應用所產生的資料，帶著不同的資料屬性與不同的環境應用需求，也產生多樣化的研究議題，持續地推進資料工程(Data Engineering)研究領域向前發展。而這樣的發展也伴隨資訊技術的更貼近日常生活而有著持續加快的趨勢。
-            </p>
-            <Button as='a' size='large'>Read More</Button>
+            <ApiDemoModal name='KCM' intro='一個會返回相關字的API' picture='https://image.flaticon.com/icons/svg/204/204358.svg' descript='輸入想查的單字'/>
 
             <Header as='h3' style={{ fontSize: '2em' }}>研究目標</Header>
             <p style={{ fontSize: '1.33em' }}>
             我們以資料工程技術為出發點考慮各式不一樣應用環境中所產生的資料進行該類型資料之查詢處理與探勘。而就近期研究規劃目標而言，我們規劃三近程研究目標：(1)社群媒體輿情分析(Social Media Analysis)，(2)行動裝置使用者行為探勘(Mobile Data Analysis)，(3)平行與分散式資料處理架構(Parallel and Distributed Data Processing)。而就遠程研究重心而言，我們將仍以資料工程知識為核心，配合於各式現有資通平台上所累積之資料分析與查詢處理經驗，朝向未來可預見之更多樣化之平台與資料類型資料管理應用發展
             </p>
             <Button as='a' size='large'>Read More</Button>
+            <ApiDemoModal name='KCM'/>
           </Container>
         </Segment>
 
